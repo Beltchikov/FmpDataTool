0fafd7ef69678f139f1f5eb9d95567c24db60165
Jogger <beltchikov@gmx.de>
2020-12-25 09:36:42 +0100

    Link

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+
+namespace FmpDataTool
+{
+    public class MainWindowViewModel : DependencyObject
+    {
+        public RelayCommand CommandRequestNavigate { get; set; }
+
+        public MainWindowViewModel()
+        {
+            CommandRequestNavigate = new RelayCommand(p => { Process.Start(new ProcessStartInfo(((Uri)p).AbsoluteUri) { UseShellExecute = true }); });
+        }
+
+
+    }
+}
