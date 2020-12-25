4eb0d5b624b6b2f27c698918b28e0823cb4f1f6d
Jogger <beltchikov@gmx.de>
2020-12-25 12:09:57 +0100

    Intermediate

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -13,6 +13,16 @@ namespace FmpDataTool
     {
         public RelayCommand CommandRequestNavigate { get; set; }
         public static readonly DependencyProperty UrlStockListProperty;
+        public static readonly DependencyProperty ResultsStockListProperty;
+
+        /// <summary>
+        /// MainWindowViewModel - Static
+        /// </summary>
+        static MainWindowViewModel()
+        {
+            UrlStockListProperty = DependencyProperty.Register("UrlStockList", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
+            ResultsStockListProperty = DependencyProperty.Register("ResultsStockList", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
+        }
 
         /// <summary>
         /// MainWindowViewModel
@@ -23,21 +33,20 @@ namespace FmpDataTool
             UrlStockList = "https://financialmodelingprep.com/api/v3/stock/list?apikey=14e7a22ed6110f130afa41af05599bb6";
         }
 
-        /// <summary>
-        /// MainWindowViewModel - Static
-        /// </summary>
-        static MainWindowViewModel()
-        {
-            UrlStockListProperty = DependencyProperty.Register("UrlStockList", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
-        }
-
-
-
         public string UrlStockList
         {
             get { return (string)GetValue(UrlStockListProperty); }
             set { SetValue(UrlStockListProperty, value); }
         }
+
+
+
+        public string ResultsStockList
+        {
+            get { return (string)GetValue(ResultsStockListProperty); }
+            set { SetValue(ResultsStockListProperty, value); }
+        }
+
 
 
 
