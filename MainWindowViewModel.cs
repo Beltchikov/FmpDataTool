7ac986c33d1e1154102f03347e4ecc8751a89f68
Jogger <beltchikov@gmx.de>
2020-12-25 13:14:31 +0100

    HttpClient call

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace FmpDataTool
@@ -13,6 +16,7 @@ namespace FmpDataTool
     {
         public static readonly DependencyProperty UrlStockListProperty;
         public static readonly DependencyProperty ResultsStockListProperty;
+        public static readonly DependencyProperty LogProperty;
 
         public RelayCommand CommandRequestNavigate { get; set; }
         public RelayCommand CommandGetStockList { get; set; }
@@ -24,6 +28,8 @@ namespace FmpDataTool
         {
             UrlStockListProperty = DependencyProperty.Register("UrlStockList", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
             ResultsStockListProperty = DependencyProperty.Register("ResultsStockList", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
+            LogProperty = DependencyProperty.Register("Log", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
+
         }
 
         /// <summary>
@@ -34,7 +40,7 @@ namespace FmpDataTool
             UrlStockList = "https://financialmodelingprep.com/api/v3/stock/list?apikey=14e7a22ed6110f130afa41af05599bb6";
 
             CommandRequestNavigate = new RelayCommand(p => { Process.Start(new ProcessStartInfo(((Uri)p).AbsoluteUri) { UseShellExecute = true }); });
-            CommandGetStockList = new RelayCommand(GetStockList);
+            CommandGetStockList = new RelayCommand(async (p) => await GetStockList(p));
         }
 
         public string UrlStockList
@@ -49,12 +55,37 @@ namespace FmpDataTool
             set { SetValue(ResultsStockListProperty, value); }
         }
 
-        private void GetStockList(object param)
+        public string Log
         {
-
+            get { return (string)GetValue(LogProperty); }
+            set { SetValue(LogProperty, value); }
         }
 
 
+        private async Task GetStockList(object param)
+        {
+            Log += "\r\nRequesting stock list...";
+
+            using var httpClient = new HttpClient();
+            await httpClient.GetAsync(UrlStockList).ContinueWith((r) => OnRequestStockListComplete(r));
+
+        }
+
+        private void OnRequestStockListComplete(Task<HttpResponseMessage> requestTask)
+        {
+            HttpResponseMessage httpResponseMessage = requestTask.Result;
+
+            // TODO
+            //var contentStream = await requestTask.Content.ReadAsStreamAsync();
+            //ResultsStockList = response.ToString();
+            Dispatcher.Invoke(SetData);
+      
+        }
+
+        private void SetData()
+        {
+            Log += "\r\nOK! stock list recieved.";
+        }
 
     }
 }
